69b7c194123d4cc91c033fc8f1a8cd64ea478659
EstebanUNRN <mezeiesteban@gmail.com>
2026-09-17 14:40:04 -0300

    optimiza cantidad_ciudades_completas de estadisticas.py

--- estadisticas.py
+++ estadisticas.py
@@ -7,7 +7,9 @@ def cantidad_ciudades_completas(observaciones: dict) -> int:
     """Devuelve la cantidad de ciudades sin ningún dato faltante."""
     contador = 0
     for datos in observaciones.values():
-        if datos["temperatura"] is not None and datos["sensacion_termica"] is not None:
+        #If None not in dict_i.values()
+        #ciudades_completas += 1
+        if None not in datos.values():
             contador += 1
     return contador
 
@@ -16,3 +18,4 @@ def top_n_ciudades(observaciones: dict, campo: str, n: int, descendente: bool = 
     """Devuelve las n (por parámetro) ciudades ordenadas según 'campo', de mayor a menor
     (o al revés si descendente=False), en una lista. Reutilizable tanto para temperatura
     como para viento."""
+
